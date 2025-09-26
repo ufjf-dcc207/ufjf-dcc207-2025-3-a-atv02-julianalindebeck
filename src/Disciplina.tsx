9d98e30a82856529cd3537cbf164fe4ae2e05729
Juliana Lindebeck <>
2025-09-25 21:40:08 -0300

    :lipstick: adiciona estilo de Disciplina

--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,6 +1,10 @@
+import "./Disciplina.css"
+
 export default function Disciplina(){
-    return <>
-        <h1>DCC07</h1>
-        <h2>Desenvolvimento Web Front-End</h2>
-    </>
+    return (
+        <div className="disciplina">
+            <h1>DCC07</h1>
+            <h2>Desenvolvimento Web Front-End</h2>
+        </div>
+    )
 }
