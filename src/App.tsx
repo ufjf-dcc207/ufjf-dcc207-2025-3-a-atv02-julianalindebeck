0026193da077f15df021872c04d09a4240de0da2
Juliana Lindebeck <>
2025-09-25 22:00:54 -0300

    :memo: adiciona tipos para Disciplina

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
     <>
       <h1>Olá, mundo!</h1>
       <h2>Olá, React!</h2>
-      <Disciplina></Disciplina>
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front-End"/>
+      <Disciplina codigo="DCC206" nome="Desenvolvimento Web II"/>
+      <Disciplina codigo="DCC202" nome="Desenvolvimento Web I"/>
     </>
   );
 }
