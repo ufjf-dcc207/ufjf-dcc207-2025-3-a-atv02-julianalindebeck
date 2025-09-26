93882d52f4585703988e1ebc02b8d44ecc2c0888
juliana.lindebeck@estudante.ufjf.br <>
2025-09-25 21:14:48 -0300

    :sparkles: adiciona componente Disciplina

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Disciplina from './Disciplina';
 
 function App() {
   return (
     <>
       <h1>Olá, mundo!</h1>
       <h2>Olá, React!</h2>
+      <Disciplina></Disciplina>
     </>
   );
 }
---
+++ src/Disciplina.tsx
@@ -0,0 +1,6 @@
+export default function Disciplina(){
+    return <>
+        <h1>DCC07</h1>
+        <h2>Desenvolvimento Web Front-End</h2>
+    </>
+}
